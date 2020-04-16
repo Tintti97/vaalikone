3cbf590671d4d65ca85c9f9503ebabe1b8b1e572
Tintti97 <tiainen97@gmail.com>
2020-04-16 17:03:57 +0300

    Tintti testi commit

--- vaalikones/src/main/java/vaalikone/Login.java
+++ vaalikones/src/main/java/vaalikone/Login.java
@@ -13,7 +13,7 @@ public class Login extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         
-        // Testi kommentti Tintti
+        //Testi kommentti Teemu =)
         
         //Saadaan parametrit login.jsp sivulta testi
         String email = request.getParameter("email");
